907c74693a7b6200796f3418aa049c76eb60d21b
Nar Saynorath <nsaynorath@securitycompass.com>
2020-08-17 15:11:35 -0400

    Fixes url to API

--- src/services/signInService.js
+++ src/services/signInService.js
@@ -10,7 +10,7 @@ class SignInService extends APIService {
       first_name: visitorInfo.first_name,
       last_name: visitorInfo.last_name,
       email: visitorInfo.email,
-      url_private: visitorInfo.url_private,
+      image_url: visitorInfo.url_private,
       chaperones: chaperoneEmails,
     };
 
